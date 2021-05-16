576aa1075bf91f2cc518fff7d0ca9d9afd4f09b0
Matt Ronchetto <hello@doamatto.xyz>
2021-05-16 13:42:30 -0700

    chore: added card component (unstyled)

--- packages/@ui-framework-react/src/components/layout/card.tsx
+++ packages/@ui-framework-react/src/components/layout/card.tsx
@@ -1,13 +1,48 @@
-import React, {
-    Fragment,
-    createContext,
-    useCallback,
-    useContext,
-    useMemo,
-    useState,
-  
-    // Types
-    ElementType,
-    KeyboardEvent as ReactKeyboardEvent,
-    MouseEvent as ReactMouseEvent,
-} from 'react'
+import React from 'react'
+import Button from '../interaction/button'
+import './card.css'
+
+interface Typings {
+    title: string,
+    image?: string,
+    subtitle: string,
+    // TODO: replace `any` with proper typings
+    buttonText?: any,
+    buttonLink?: any
+}
+
+interface WithButton extends Typings {
+    buttonText: string,
+    buttonLink?: string
+}
+
+type CardType = (Typings & WithButton) | Typings
+
+export default function Card({
+    title,
+    image,
+    subtitle,
+    buttonText,
+    buttonLink
+} : CardType) {
+    return (
+        <div className="card">
+            <img src={image} className="card.image" />
+            <div className="card.text">
+                <h2 className="card.title">{title}</h2>
+                <p className="card.subtitle">{subtitle}</p>
+                {buttonLink && buttonText && (
+                    <Button
+                        title={buttonText}
+                        link={buttonLink}
+                    />
+                )}
+                {buttonText && (
+                    <Button
+                        title={buttonText}
+                    />
+                )}
+            </div>
+        </div>
+    )
+}
