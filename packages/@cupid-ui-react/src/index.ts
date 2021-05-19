eb6b64387ec794fc0bb5cee6090eaa1c7125c34e
Matt Ronchetto <hello@doamatto.xyz>
2021-05-19 14:45:57 -0700

    chore: add button

--- packages/@cupid-ui-react/src/index.ts
+++ packages/@cupid-ui-react/src/index.ts
@@ -1,5 +1,6 @@
 import 'cupid-ui/ui-framework.css'
 
+export * from './components/interaction/button'
 export * from './components/layout/card'
 
 export * as UIColors from './components/vars/colours'
