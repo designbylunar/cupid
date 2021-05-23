54158193650a4674a520a4e82682194a453351de
Matt Ronchetto <hello@doamatto.xyz>
2021-05-23 08:34:21 -0700

    chore: add variant support for button

--- react/src/components/interaction/button.tsx
+++ react/src/components/interaction/button.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 
 interface ButtonType {
     title: string,
-    link?: string
+    link?: string,
+    type?: string
 }
+
+const ButtonTypes = [
+  "mono"
+]
 
 export function Button({
     title,
-    link
+    link,
+    type
 } : ButtonType) {
+  if (type !== undefined)
+    if (ButtonTypes.includes(type) === false)
+      throw Error("Invalid \"type\" property provided to Cupid UI's Button component. More information may follow.")
+
     if (link) return (
-        <a href={link} title={title} className="button">
+        <a href={link} title={title} className={type ? `button.${type}` : "button"}>
             {title}
         </a>
     )
