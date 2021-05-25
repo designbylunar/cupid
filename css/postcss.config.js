7ea0c7ae72bbe445515c577e003ff92b8bbfe02e
Matt Ronchetto <hello@doamatto.xyz>
2021-05-25 10:51:37 -0700

    deps: see last commit

--- css/postcss.config.js
+++ css/postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-each': {},
-    'postcss-preset-env': {},
     'postcss-nested': {},
     cssnano: {},
     autoprefixer: {},
