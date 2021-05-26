370de164a097c9f033e1165d9226c24028f43893
Matt Ronchetto <hello@doamatto.xyz>
2021-05-26 16:01:34 -0700

    chore: more refactoring for parcel

--- react/playground/app.js
+++ react/playground/app.js
@@ -1,27 +1,31 @@
-import {Card, Button} from '../src/index';
+import { h } from 'preact'
 
-import './style.css';
-import '../../node_modules/@cupid-ui/css/dist/cupid-ui.css';
+import {Card, Button} from '../src/index'
 
-export default App() (
-  <div>
-    <h1>Hello, World!</h1>
-    <h2>Buttons</h2>
-    <Button 
-      title="Blow up the world"
-      link="#"
-      type=""
-    />
-    <h2>Card</h2>
-    <Card
-      title="George"
-      subtitle="Professional chaos causer"
-      buttonText="Hire George"
-      buttonLink="#"
-      image="placeholder.jpg"
-      imageWidth="128"
-      imageHeight="128"
-      imageDesc="Picture of George, a professional chaos causer. Yes, he's a goose."
-    />
-  </div>
-)
+import './style.css'
+import '../../node_modules/@cupid-ui/css/dist/cupid-ui.css'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>Buttons</h2>
+      <Button 
+        title="Blow up the world"
+        link="#"
+        type=""
+      />
+      <h2>Card</h2>
+      <Card
+        title="George"
+        subtitle="Professional chaos causer"
+        buttonText="Hire George"
+        buttonLink="#"
+        image="placeholder.jpg"
+        imageWidth="128"
+        imageHeight="128"
+        imageDesc="Picture of George, a professional chaos causer. Yes, he's a goose."
+      />
+    </div>
+  )
+}
