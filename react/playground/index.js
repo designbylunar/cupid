167dd4b1d583559df7163bfacb291a28be9dd9f6
Matt Ronchetto <hello@doamatto.xyz>
2021-05-26 17:53:27 -0700

    chore: fix Parcel server

--- react/playground/index.js
+++ react/playground/index.js
@@ -1,6 +1,36 @@
-import { render } from 'preact'
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = document.getElementById('root')
+import './style.css'
+import '../../node_modules/@cupid-ui/css/dist/cupid-ui.css'
+import {Card, Button} from '../src/index'
 
-render(<App />, root)
+class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <h2>Buttons</h2>
+        <Button 
+          title="Blow up the world"
+          link="#"
+          type=""
+        />
+        <h2>Card</h2>
+        <Card
+          title="George"
+          subtitle="Professional chaos causer"
+          buttonText="Hire George"
+          buttonLink="#"
+          image="placeholder.jpg"
+          imageWidth="128"
+          imageHeight="128"
+          imageDesc="Picture of George, a professional chaos causer. Yes, he's a goose."
+        />
+      </div>
+    )
+  }
+}
+
+var mountNode = document.getElementById("root")
+ReactDOM.render(<Page/>, mountNode)
