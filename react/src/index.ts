fa29918b171f1d819255d7dbff8b2f96e0b8102e
Matt Ronchetto <hello@doamatto.xyz>
2021-05-26 19:17:25 -0700

    chore: add Alerts to React

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,4 +1,6 @@
 export * from './components/interaction/button'
+
+export * from './components/layout/alert'
 export * from './components/layout/card'
 
 export * as UIColors from './components/vars/colours'
