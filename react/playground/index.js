75ad95038f656355c57fce07edc8b3b8cc99be17
Matt Ronchetto <hello@doamatto.xyz>
2021-05-26 18:09:40 -0700

    fix()

--- react/playground/index.js
+++ react/playground/index.js
@@ -1,37 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './style.css'
-import '../../css/dist/cupid-ui.css'
-import {Card, Button} from '../src/index'
+import './style.css';
+import '../../css/dist/cupid-ui.css';
+import { Card, Button } from '../src/index';
 
-import placeholder from 'url:./placeholder.jpg?as=webp'
+import placeholder from 'url:./placeholder.jpg?as=webp';
 
-class Page extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-        <h2>Buttons</h2>
-        <Button 
-          title="Blow up the world"
-          link="#"
-        />
-        <h2>Card</h2>
-        <Card
-          title="George"
-          subtitle="Professional chaos causer"
-          buttonText="Hire George"
-          buttonLink="#"
-          image={placeholder}
-          imageWidth="128"
-          imageHeight="128"
-          imageDesc="Picture of George, a professional chaos causer. Yes, he's a goose."
-        />
-      </div>
-    )
-  }
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <h2>Buttons</h2>
+      <Button
+        title="Blow up the world"
+        link="#"
+      />
+      <h2>Card</h2>
+      <Card
+        title="George"
+        subtitle="Professional chaos causer"
+        buttonText="Hire George"
+        buttonLink="#"
+        image={placeholder}
+        imageWidth="128"
+        imageHeight="128"
+        imageDesc="Picture of George, a professional chaos causer. Yes, he's a goose."
+      />
+    </div>
+  );
 }
 
-var mountNode = document.getElementById("root")
-ReactDOM.render(<Page/>, mountNode)
+const mountNode = document.getElementById('root');
+ReactDOM.render(<Page />, mountNode);
