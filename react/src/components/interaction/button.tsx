cf986cfac60a3844d6113200873b92bd01bbebc2
Matt Ronchetto <hello@doamatto.xyz>
2021-05-26 19:35:43 -0700

    chore: remove fluff on JSdoc

--- react/src/components/interaction/button.tsx
+++ react/src/components/interaction/button.tsx
@@ -11,7 +11,7 @@ interface ButtonType {
  * @param title The text displayed on the button
  * @param link The link the button goes to
  * @param {("mono")} type The type of button you want. You can use this optional type to change it to a documented theme such as "mono"
- * @returns button element
+ * @returns
  */
 export function Button({
     title,
