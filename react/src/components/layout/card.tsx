847ae2cf5a8b7beedb195863171373a7a99861ee
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-07-17 11:28:02 -0700

    docs: update JSDoc with typings, optionals. removed button mentions

--- react/src/components/layout/card.tsx
+++ react/src/components/layout/card.tsx
@@ -23,15 +23,13 @@ type CardType = (Typings & WithImage) | Typings
 
 /**
  * Creates a card element
- * @param title The main text of the card (eg. the name of a project or person)
- * @param subtitle The text below the title. (eg. blurb about project or a person's role at a company)
- * @param buttonText The text displayed on the button
- * @param buttonLink The link the button goes to
- * @param {("mono")} buttonType The type associated with the button
- * @param image The image that appears on the card
- * @param imageWidth The width of the image aforementioned. This must be defined to prevent layout shift.
- * @param imageHeight The height of the image aforementioned. This must be defined to prevent layout shift.
- * @param imageDesc A short description of what's in the image  
+ * @param {string} title The main text of the card (eg. the name of a project or person)
+ * @param {string} subtitle The text below the title. (eg. blurb about project or a person's role at a company)
+ * @param {boolean} [reverse] If true, the order will be flipped (text and then the image). This has no effect if you are not using an image. 
+ * @param {string} image The image that appears on the card
+ * @param {string} imageWidth The width of the image aforementioned. This must be defined to prevent layout shift.
+ * @param {string} imageHeight The height of the image aforementioned. This must be defined to prevent layout shift.
+ * @param {string} imageDesc A short description of what's in the image
  * @returns 
  */
 export function Card({
