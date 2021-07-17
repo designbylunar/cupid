5e7fa4a62b48db5598841bf3a451e030d165abd3
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-07-17 11:26:22 -0700

    fix: remove missing import (colours aren't bundled via JS variables, but CSS variables through CSS package)

--- react/src/index.ts
+++ react/src/index.ts
@@ -4,5 +4,3 @@ export * from './components/interaction/modal'
 export * from './components/layout/alert'
 export * from './components/layout/badge'
 export * from './components/layout/card'
-
-export * as UIColors from './components/vars/colours'
