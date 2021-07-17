db7a8fb44af731e4cea283dd8340d21818d0e3d2
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-07-17 11:25:13 -0700

    docs: added typings and optionals

--- react/src/components/layout/alert.tsx
+++ react/src/components/layout/alert.tsx
@@ -8,9 +8,9 @@ interface Typings {
 
 /**
  * Creates an Alert, that can be used with other tools
- * @param title This goes before the rest of the text and is in bold. Solid for a quick detail, followed by more elaborate information about the alert
- * @param text This is what the alert says
- * @param {('success'|'warning'|'error'|'info')} type This is the kind of alert. You can use a
+ * @param {string} [title] This goes before the rest of the text and is in bold. Solid for a quick detail, followed by more elaborate information about the alert
+ * @param {string} text This is what the alert says
+ * @param {('success'|'warning'|'error'|'info')} [type] This is the kind of alert. You can use a
  * @returns
 */
 export function Alert({
--- react/src/components/layout/badge.tsx
+++ react/src/components/layout/badge.tsx
@@ -7,8 +7,8 @@ interface Typings {
 
 /**
  * Creates a Badge, that can be used with other components
- * @param text This is what the badge says.
- * @param {('success'|'warning'|'error'|'info')} type Sets the type of badge you want.
+ * @param {string} text This is what the badge says.
+ * @param {('success'|'warning'|'error'|'info')} [type] Sets the type of badge you want.
  * @returns
 */
 export function Badge({
