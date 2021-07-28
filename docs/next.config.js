a6b3ce1eccd27c4a54e6ace5bfe7d268edc1e438
Matt Ronchetto <hello@doamatto.xyz>
2021-07-27 17:08:50 -0700

    chore: add asset prefix

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,3 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 module.exports = withNextra()
-module.exports = { basePath: '/cupid' }
+module.exports = {
+  basePath: '/cupid',
+  assetPrefix: '/cupid/,
+}
